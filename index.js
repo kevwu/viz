304e06836e5ffd469c74503aa49cdc9d9ace9836
Kevin Wu <kevin@kevinywu.com>
2017-11-30 17:50:03 -0600

    Add HTTPS server (for microphone access)

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const https = require("https")
 const express = require("express")
 const app = express()
+const fs = require("fs")
 
 const path = require("path")
 
@@ -9,5 +11,13 @@ app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, 'static/index.html'))
 })
 
-
 app.listen(3000, () => {console.log("HTTP server listening")})
+
+https.createServer({
+	key: fs.readFileSync('./ssl/server.key'),
+	cert: fs.readFileSync('./ssl/server.crt'),
+	requestCert: false,
+	rejectUnauthorized: false
+}, app).listen(3001, () => {
+	console.log("HTTPS server listening")
+})
